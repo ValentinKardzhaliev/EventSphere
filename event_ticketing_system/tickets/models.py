e4a9930b1d449fab07689fed592d4495821182a9
ValentinKardzhaliev <vkardjaliev@abv.bg>
2023-12-17 19:52:50 +0200

    Added the ticket model, which includes both VIP and Regular tickets

--- event_ticketing_system/tickets/models.py
+++ event_ticketing_system/tickets/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Ticket(models.Model):
+    TICKET_TYPE_CHOICES = [
+        ('regular', 'Regular Ticket'),
+        ('vip', 'VIP Ticket'),
+    ]
+
+    event = models.ForeignKey('events.Event', on_delete=models.CASCADE, related_name='tickets')
+    ticket_type = models.CharField(max_length=10, choices=TICKET_TYPE_CHOICES)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity_available = models.PositiveIntegerField(default=0)
