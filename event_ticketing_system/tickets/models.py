af24fc3af0991584ec7453fc095c99206a80637d
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-01-22 19:46:19 +0200

    Removed ticket_type for now

--- event_ticketing_system/tickets/models.py
+++ event_ticketing_system/tickets/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Ticket(models.Model):
-    TICKET_TYPE_CHOICES = [
-        ('regular', 'Regular Ticket'),
-        ('vip', 'VIP Ticket'),
-    ]
+    # TICKET_TYPE_CHOICES = [
+    #     ('regular', 'Regular Ticket'),
+    #     ('vip', 'VIP Ticket'),
+    # ]
 
     event = models.ForeignKey('events.Event', on_delete=models.CASCADE, related_name='tickets')
-    ticket_type = models.CharField(max_length=10, choices=TICKET_TYPE_CHOICES)
+    # ticket_type = models.CharField(max_length=10, choices=TICKET_TYPE_CHOICES)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity_available = models.PositiveIntegerField(default=0)
