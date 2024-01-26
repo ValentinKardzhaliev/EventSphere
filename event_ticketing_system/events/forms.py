75de62931e1668791b5c24af5a14fce7f7e9f01d
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-01-26 19:34:24 +0200

    Fixed the Ticket model and the create event form

--- event_ticketing_system/events/forms.py
+++ event_ticketing_system/events/forms.py
@@ -1,36 +1,62 @@
 from django import forms
-from django.forms import inlineformset_factory
 
 from .models import Event
 from ..tickets.models import Ticket
 
 
+# class EventAddForm(forms.ModelForm):
+#     class Meta:
+#         model = Event
+#         fields = [
+#             'title',
+#             'description',
+#             'date_and_time',
+#             'venue',
+#             'category',
+#             'contact_information',
+#             'organizer',
+#             'image',
+#         ]
+#
+#  widgets = {
+#     'date_and_time': forms.DateTimeInput(
+#        attrs={'placeholder': 'YYYY-MM-DD HH:MM', 'type': 'datetime-local'}),
+# }
+#
+#     tickets = inlineformset_factory(Event, Ticket, fields=('ticket_type', 'price', 'quantity_available'), extra=1,
+#                                     can_delete=False)
+#
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         # Add any additional customization for the form fields here if needed
+#
+#     def clean_date_and_time(self):
+#         # Additional cleaning logic for date_and_time if needed
+#         return self.cleaned_data['date_and_time']
+
 class EventAddForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = [
-            'title',
-            'description',
-            'date_and_time',
-            'venue',
-            'category',
-            'contact_information',
-            'organizer',
-            'image',
-        ]
+        fields = ['title', 'description', 'date_and_time', 'venue', 'category', 'contact_information', 'organizer',
+                  'image']
 
-        widgets = {
-            'date_and_time': forms.DateTimeInput(
-                attrs={'placeholder': 'YYYY-MM-DD HH:MM', 'type': 'datetime-local'}),
-        }
+    # Additional fields for ticket creation
+    vip_quantity = forms.IntegerField(label='VIP Tickets Quantity', min_value=0, required=True)
+    regular_quantity = forms.IntegerField(label='Regular Tickets Quantity', min_value=0, required=True)
 
-    tickets = inlineformset_factory(Event, Ticket, fields=('ticket_type', 'price', 'quantity_available'), extra=1,
-                                    can_delete=False)
+    widgets = {
+        'date_and_time': forms.DateTimeInput(
+            attrs={'placeholder': 'YYYY-MM-DD HH:MM', 'type': 'datetime-local'}),
+    }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Add any additional customization for the form fields here if needed
+    def save(self, commit=True):
+        event = super(EventAddForm, self).save(commit)
 
-    def clean_date_and_time(self):
-        # Additional cleaning logic for date_and_time if needed
-        return self.cleaned_data['date_and_time']
+        # Create VIP and Regular tickets for the event
+        vip_quantity = self.cleaned_data.get('vip_quantity')
+        regular_quantity = self.cleaned_data.get('regular_quantity')
+
+        Ticket.objects.create(event=event, ticket_type=Ticket.VIP, quantity=vip_quantity)
+        Ticket.objects.create(event=event, ticket_type=Ticket.REGULAR, quantity=regular_quantity)
+
+        return event
--- event_ticketing_system/tickets/models.py
+++ event_ticketing_system/tickets/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
+from event_ticketing_system.events.models import Event
+
 
 class Ticket(models.Model):
-    # TICKET_TYPE_CHOICES = [
-    #     ('regular', 'Regular Ticket'),
-    #     ('vip', 'VIP Ticket'),
-    # ]
+    VIP = 'VIP'
+    REGULAR = 'Regular'
 
-    event = models.ForeignKey('events.Event', on_delete=models.CASCADE, related_name='tickets')
-    # ticket_type = models.CharField(max_length=10, choices=TICKET_TYPE_CHOICES)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    quantity_available = models.PositiveIntegerField(default=0)
+    TICKET_TYPE_CHOICES = [
+        (VIP, 'VIP'),
+        (REGULAR, 'Regular'),
+    ]
+
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='tickets')
+    ticket_type = models.CharField(max_length=10, choices=TICKET_TYPE_CHOICES)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.event.title} - {self.ticket_type} Ticket"
+
+    class Meta:
+        unique_together = ('event', 'ticket_type')
