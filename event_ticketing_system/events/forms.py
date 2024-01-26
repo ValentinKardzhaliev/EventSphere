6918a535f1f7b62911cb03158083ab78a2b68224
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-01-26 19:38:00 +0200

    Added the price for tickets in the add event form

--- event_ticketing_system/events/forms.py
+++ event_ticketing_system/events/forms.py
@@ -1,38 +1,8 @@
+# In forms.py
 from django import forms
-
 from .models import Event
 from ..tickets.models import Ticket
 
-
-# class EventAddForm(forms.ModelForm):
-#     class Meta:
-#         model = Event
-#         fields = [
-#             'title',
-#             'description',
-#             'date_and_time',
-#             'venue',
-#             'category',
-#             'contact_information',
-#             'organizer',
-#             'image',
-#         ]
-#
-#  widgets = {
-#     'date_and_time': forms.DateTimeInput(
-#        attrs={'placeholder': 'YYYY-MM-DD HH:MM', 'type': 'datetime-local'}),
-# }
-#
-#     tickets = inlineformset_factory(Event, Ticket, fields=('ticket_type', 'price', 'quantity_available'), extra=1,
-#                                     can_delete=False)
-#
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         # Add any additional customization for the form fields here if needed
-#
-#     def clean_date_and_time(self):
-#         # Additional cleaning logic for date_and_time if needed
-#         return self.cleaned_data['date_and_time']
 
 class EventAddForm(forms.ModelForm):
     class Meta:
@@ -42,21 +12,21 @@ class EventAddForm(forms.ModelForm):
 
     # Additional fields for ticket creation
     vip_quantity = forms.IntegerField(label='VIP Tickets Quantity', min_value=0, required=True)
+    vip_price = forms.DecimalField(label='VIP Ticket Price', min_value=0, required=True)
     regular_quantity = forms.IntegerField(label='Regular Tickets Quantity', min_value=0, required=True)
-
-    widgets = {
-        'date_and_time': forms.DateTimeInput(
-            attrs={'placeholder': 'YYYY-MM-DD HH:MM', 'type': 'datetime-local'}),
-    }
+    regular_price = forms.DecimalField(label='Regular Ticket Price', min_value=0, required=True)
 
     def save(self, commit=True):
         event = super(EventAddForm, self).save(commit)
 
         # Create VIP and Regular tickets for the event
         vip_quantity = self.cleaned_data.get('vip_quantity')
+        vip_price = self.cleaned_data.get('vip_price')
         regular_quantity = self.cleaned_data.get('regular_quantity')
+        regular_price = self.cleaned_data.get('regular_price')
 
-        Ticket.objects.create(event=event, ticket_type=Ticket.VIP, quantity=vip_quantity)
-        Ticket.objects.create(event=event, ticket_type=Ticket.REGULAR, quantity=regular_quantity)
+        Ticket.objects.create(event=event, ticket_type=Ticket.VIP, quantity=vip_quantity, price_per_ticket=vip_price)
+        Ticket.objects.create(event=event, ticket_type=Ticket.REGULAR, quantity=regular_quantity,
+                              price_per_ticket=regular_price)
 
         return event
