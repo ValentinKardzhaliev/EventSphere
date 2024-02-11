3f34de3fde4c57351ef570d3979ff4f9074d49b8
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-02-11 17:58:26 +0200

    Added a placeholder for the price per ticket field

--- event_ticketing_system/tickets/forms.py
+++ event_ticketing_system/tickets/forms.py
@@ -7,6 +7,9 @@ class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ['ticket_type', 'quantity_available', 'price_per_ticket']
+        widgets = {
+            'price_per_ticket': forms.NumberInput(attrs={'step': '1.00', 'placeholder': 'Price per ticket in $'}),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
