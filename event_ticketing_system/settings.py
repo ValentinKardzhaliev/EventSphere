30e0ff32cd9e8dd3ae3f27f0e443fbba5d2fd1a7
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-03-10 17:48:49 +0200

    Made errors show up in the register page and the invalid fields to have a red border

--- event_ticketing_system/settings.py
+++ event_ticketing_system/settings.py
@@ -81,20 +81,21 @@ CITIES_LIGHT_TRANSLATION_LANGUAGES = ['en']
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         'NAME': 'django.contrib.web_auth.password_validation.UserAttributeSimilarityValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.web_auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.web_auth.password_validation.CommonPasswordValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.web_auth.password_validation.NumericPasswordValidator',
-#     },
-# ]
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 
 
 LANGUAGE_CODE = 'en-us'
