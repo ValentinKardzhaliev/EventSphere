4d3d3520040db79128810dd36d4900ab83fd6333
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-05-14 20:03:12 +0300

    Added the User model to the django admin

--- event_ticketing_system/web_auth/admin.py
+++ event_ticketing_system/web_auth/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from .models import EventAppUser
 
-# Register your models here.
+
+class EventAppUserAdmin(auth_admin.UserAdmin):
+    list_display = ('username', 'email', 'balance')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email', 'profile_picture', 'balance')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'profile_picture', 'balance'),
+        }),
+    )
+
+
+admin.site.register(EventAppUser, EventAppUserAdmin)
